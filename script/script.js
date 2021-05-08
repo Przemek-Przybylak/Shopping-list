c6212f4a02a9b209741e5fb1f25135329ad2e969
Przemyslaw Przybylak <przemyslaw.przybylak2@gmail.com>
2021-05-08 12:11:53 +0200

    remove task function added

--- script/script.js
+++ script/script.js
@@ -1,31 +1,43 @@
 {
-    const tasks = [
-        
-    ]
-
-    const render = () => {
-        let htmlString = "";
-        
-
-        for (const task of tasks) {
-            htmlString += `
-            <li  class="section__item ${task.done ? "section__itemDone" : ""}"> 
-            <button class="section__image section__image--green"></button>
-            ${task.content}
-            <button class="section__image section__image--red"></button> 
-            </li>
-            `;
-        }
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    }
+    const tasks = []
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
             done: false,
         });
+
         render();
-    }
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    };
+
+    const render = () => {
+        let htmlString = "";
+        
+        for (const task of tasks) {
+            htmlString += `
+            <li  class="section__item ${task.done ? "section__itemDone" : ""}"> 
+            <button class="section__image section__image--green"></button>
+            ${task.content}
+            <button class="section__image section__image--red js-remove"></button> 
+            </li>
+            `;
+        
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+               removeTask(index);
+            });
+        });
+    };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
