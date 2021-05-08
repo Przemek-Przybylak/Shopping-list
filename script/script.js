29bfccc6686aea33a400d0b2812852ce54d70043
Przemyslaw Przybylak <przemyslaw.przybylak2@gmail.com>
2021-05-08 12:53:50 +0200

    separate binding events function, added strikeout

--- script/script.js
+++ script/script.js
@@ -10,18 +10,41 @@
         render();
     };
 
-    const removeTask = (index) => {
-        tasks.splice(index, 1);
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
         render();
     };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, taskIndex) => {
+            removeButton.addEventListener("click", () => {
+               removeTask(taskIndex);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-taskDone");
+
+        toggleDoneButtons.forEach((toggleDoneButton, taskIndex) => {
+            toggleDoneButton.addEventListener("click", () => {
+               toggleTaskDone(taskIndex);
+            });
+        });
+    }
 
     const render = () => {
         let htmlString = "";
         
         for (const task of tasks) {
             htmlString += `
-            <li  class="section__item ${task.done ? "section__itemDone" : ""}"> 
-            <button class="section__image section__image--green"></button>
+            <li  class="section__item ${task.done ? "section__item--strikeout " : ""}"> 
+            <button class="section__image section__image--green ${task.done ? "section__itemDone" : ""} js-taskDone"></button>
             ${task.content}
             <button class="section__image section__image--red js-remove"></button> 
             </li>
@@ -30,13 +53,8 @@
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
-        const removeButtons = document.querySelectorAll(".js-remove");
+        bindEvents();
 
-        removeButtons.forEach((removeButton, index) => {
-            removeButton.addEventListener("click", () => {
-               removeTask(index);
-            });
-        });
     };
 
     const onFormSubmit = (event) => {
